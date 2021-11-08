307338bc2de92957bf60832081e35e62ff93d0af
YkSlavik <81549171+YkSlavik@users.noreply.github.com>
2021-11-08 16:34:30 -0500

    a

--- monopolyService.js
+++ monopolyService.js
@@ -42,6 +42,9 @@ router.get("/players/:id", readPlayer);
 router.put("/players/:id", updatePlayer);
 router.post('/players', createPlayer);
 router.delete('/players/:id', deletePlayer);
+router.get("/players/scores", readPlayerScores);
+
+
 
 app.use(router);
 app.use(errorHandler);
@@ -116,4 +119,17 @@ function deletePlayer(req, res, next) {
         .catch(err => {
             next(err);
         });
+}
+
+function readPlayerScores(req, res, next) {
+  db.oneOrNone('SELECT score
+  FROM Player, PlayerGame
+  WHERE Player.ID = PlayerGame.playerID
+    AND Player.name = 'Dogbreath'', req.params)
+      .then(data => {
+          returnDataOr404(res, data);
+      })
+      .catch(err => {
+          next(err);
+      });
 }
